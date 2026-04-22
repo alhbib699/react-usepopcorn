0f930aed2133dfb424c1b3ccb5cb53f895de5981
alhbib699 <alhbib699@gmail.com>
2026-04-22 22:25:23 +0300

    implement watched movies list and star rating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// import StarRating from "./StartRating"
+import StarRating from "./StartRating"
 
 const tempMovieData = [
   {
@@ -133,7 +133,7 @@ export default function App() {
           ) : (
             <>
               {/* <WatchedSummary watched={watched} /> */}
-              {/* <WatchedMoviesList watched={watched} onDeleteWatched={handleDeleteWatched}/> */}
+              <WatchedMoviesList watched={watched} onDeleteWatched={handleDeleteWatched}/>
             </>
           )}
         </Box>
@@ -368,30 +368,30 @@ const watchedUserRating = watched.find((movie)=> movie.imdbID === selectedId)?.u
 //   );
 // }
 
-// function WatchedMoviesList({ watched , onDeleteWatched}) {
-//   return (
-//     <ul className="list">
-//       {watched.map((movie) => (
-//         <li key={movie.imdbID}>
-//           <img src={movie.poster} alt={`${movie.title} poster`} />
-//           <h3>{movie.title}</h3>
-//           <div>
-//             <p>
-//               <span>⭐️</span>
-//               <span>{movie.imdbRating}</span>
-//             </p>
-//             <p>
-//               <span>🌟</span>
-//               <span>{movie.userRating}</span>
-//             </p>
-//             <p>
-//               <span>⏳</span>
-//               <span>{movie.runtime} min</span>
-//             </p>
-//           </div>
-//           <button className="btn-delete" onClick={()=>onDeleteWatched(movie.imdbID)}>X</button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
+function WatchedMoviesList({ watched , onDeleteWatched}) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <li key={movie.imdbID}>
+          <img src={movie.poster} alt={`${movie.title} poster`} />
+          <h3>{movie.title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+          <button className="btn-delete" onClick={()=>onDeleteWatched(movie.imdbID)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
